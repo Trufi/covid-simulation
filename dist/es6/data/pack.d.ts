c7237be11fbd83cc617d231da3a62fb63b8fa8dd
Trufi <stevemyz@gmail.com>
2022-08-06 17:00:44 +0000

    Deploying to gh-pages from @ Trufi/covid-simulation@628032c6caf41068ef5c83cfe7c1b1141830eade 🚀

--- dist/es6/data/pack.d.ts
+++
@@ -1,3 +0,0 @@
-import { Graph } from './types';
-export declare function packGraph(graph: Graph): void;
-export declare function unpackGraph(graph: Graph): void;
--- dist/es6/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Simulation } from './simulation';
-export { SimulationOptions, SimulationIcons, SimulationStartOptions, SimulationStat, SimulationFilterOptions, } from './types';
